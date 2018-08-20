a7358f575c48567ec2fc192b44293c4af9d9dd6b
swojeet <swojeetkayastha@gmail.com>
2018-08-20 11:31:39 +0545

    Add authentication via github and facebook

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
@@ -10,6 +11,10 @@ class App extends React.Component {
   state = {
     fishes: {},
     order: {}
+  };
+
+  static propTypes = {
+    match: PropTypes.object
   };
 
   componentDidMount() {
@@ -111,6 +116,7 @@ class App extends React.Component {
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
           deleteFish={this.deleteFish}
+          storeId={this.props.match.params.storeId}
         />
       </div>
     );
